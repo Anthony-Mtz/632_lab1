ba64a59f58af332370a70f8e3dcd37de0cf1ef20
RiceNinja97 <littleninja9@gmail.com>
2023-02-13 15:54:45 -0500

    modified ring oscillator

--- fast_oscil.sv
+++ fast_oscil.sv
@@ -1,12 +1,16 @@
 
 
-module fast_oscil #(parameter NUM_OSCIL = 5)(input clk, input sel_in_data, input d, output q);
+module fast_oscil #(parameter NUM_OSCIL = 5)
+	(input logic clk, sel_in_data, d, en, 
+	 output logic q);
 
 	genvar i;
 	logic [NUM_OSCIL-1:0] tmp_arr;
-	assign tmp_arr[0] = (sel_in_data) ? d : ~q;
+	logic q_tmp;
+	assign tmp_arr[0] = ~(sel_in_data & en) ? d : tmp_arr[i-1];
 	always_ff @(posedge clk) begin
-		q <= tmp_arr[NUM_OSCIL-1];
+		q_tmp <= tmp_arr[NUM_OSCIL-1];
+		q <= q_tmp
 	end
 	
 
@@ -14,9 +18,7 @@ module fast_oscil #(parameter NUM_OSCIL = 5)(input clk, input sel_in_data, input
 	
 	generate 
 		for (i = 1; i < NUM_OSCIL; i = i+1) begin : oscillator
-		
 				assign tmp_arr[i] = ~tmp_arr[i-1];
-			
 		end
 	endgenerate
 
