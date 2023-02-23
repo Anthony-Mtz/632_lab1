26d2e3d6b85e48574f23901e609659e2393bea56
Anthony-Mtz <amartin2@andrew.cmu.edu>
2023-02-23 11:40:18 -0500

    adding images for all configs

--- TRNG_image.py
+++ TRNG_image.py
@@ -3,39 +3,55 @@ import os
 import numpy as np
 from PIL import Image as im
 
-  
-# create a numpy array
-# np.uint8 is a data type containing
-# numbers ranging from 0 to 255 
-# and no non-negative integers
-size = 1000000
-array = np.arange(0, size, 1, np.uint8)
+def create_image(data_file, outpath):    
+    # create a numpy array
+    # np.uint8 is a data type containing
+    # numbers ranging from 0 to 255 
+    # and no non-negative integers
+    size = 1000000
+    array = np.arange(0, size, 1, np.uint8)
 
 
-# image file
-data_name = 'output_ring_1'
-data_file = os.path.join('results/', data_name+'.txt')
+    # image file
+    # data_name = 'output_ring_1'
+    # data_file = os.path.join('results/', data_name+'.txt')
 
-with open(data_file, 'r') as f:
-    for i in range(len(array)):
-        array[i] = 255 if f.read(1)=='1' else 0
+    #results/output_ring_1.txt
+    data_name = data_file.split('/')[1].split('.')[0]
 
-    
-# Reshape the array into a 
-# familiar resoluition --> kept 1000x1000 instead
-array = np.reshape(array, (1000, 1000))
-    
-# show the shape of the array
-print(array.shape)
+    with open(data_file, 'r') as f:
+        for i in range(len(array)):
+            array[i] = 255 if f.read(1)=='1' else 0
 
-# show the array
-print(array)
-    
-# creating image object of
-# above array
-data = im.fromarray(array)
-    
-# saving the final output 
-# as a PNG file
-outfile = os.path.join('results/', data_name+'.png')
-data.save(outfile)
+        
+    # Reshape the array into a 
+    # familiar resoluition --> kept 1000x1000 instead
+    array = np.reshape(array, (1000, 1000))
+        
+    # show the shape of the array
+    print(array.shape)
+
+    # show the array
+    print(array)
+        
+    # creating image object of
+    # above array
+    data = im.fromarray(array)
+        
+    # saving the final output 
+    # as a PNG file
+    outfile = os.path.join(outpath, data_name+'.png')
+    data.save(outfile)
+
+
+
+
+# Paths
+TRNG_data_path = 'TRNG_data'
+outpath = 'TRNG_data/pictures'
+
+# Generate all images
+for file_name in os.listdir(TRNG_data_path):
+    if(file_name.endswith('.txt')):
+        datafile = os.path.join(TRNG_data_path, file_name)
+        create_image(datafile, outpath)
